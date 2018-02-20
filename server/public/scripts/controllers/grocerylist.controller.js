abce999e7e8337eff383d674acf05bce72e692d0
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-02-19 18:12:06 -0600

    refactored logic and added in quantities.js

--- server/public/scripts/controllers/grocerylist.controller.js
+++ server/public/scripts/controllers/grocerylist.controller.js
@@ -6,6 +6,8 @@ myApp.controller('GroceryListController', ['RecipeService', function(RecipeServi
   self.addedRecipes = [];
   self.ingredients = [];
   self.refrigerator = [];
+  self.freezer = [];
+  self.pantry = [];
 
   self.addRecipesToList = function(recipe) {
     self.addedRecipes.push(recipe.recipeName);
@@ -20,43 +22,45 @@ myApp.controller('GroceryListController', ['RecipeService', function(RecipeServi
     // console.log('Sorted to categories(fridge)', sortedIngredients.refrigerator);
 
     let sortedFridgeNoDuplicates = removeDups(sortedIngredients.refrigerator, 'ingredientName');
+    let sortedFreezerNoDuplicates = removeDups(sortedIngredients.freezer, 'ingredientName');
+    let sortedPantryNoDuplicates = removeDups(sortedIngredients.pantry, 'ingredientName');
     // console.log('Fridge no duplicates', sortedFridgeNoDuplicates);
 
     let sortedFridgeByIngName = sortByProp(sortedIngredients.refrigerator, sortedFridgeNoDuplicates, 'ingredientName');
+    let sortedFreezerByIngName = sortByProp(sortedIngredients.freezer, sortedFreezerNoDuplicates, 'ingredientName');
+    let sortedPantryByIngName = sortByProp(sortedIngredients.pantry, sortedPantryNoDuplicates, 'ingredientName');
     // console.log('ingredients sorted by name', sortedFridgeByIngName);
 
     let fridgeMeasurementsNoDuplicates= getMeasurements(sortedFridgeByIngName);
+    let freezerMeasurementsNoDuplicates= getMeasurements(sortedFreezerByIngName);
+    let pantryMeasurementsNoDuplicates= getMeasurements(sortedPantryByIngName);
     // console.log('Measurement sort ',fridgeMeasurementsNoDuplicates);
 
     let fridgeQuantities = calculateQuantities(fridgeMeasurementsNoDuplicates.sortedMeasurements, fridgeMeasurementsNoDuplicates.noDuplicates);
+    let freezerQuantities = calculateQuantities(freezerMeasurementsNoDuplicates.sortedMeasurements, freezerMeasurementsNoDuplicates.noDuplicates);
+    let pantryQuantities = calculateQuantities(pantryMeasurementsNoDuplicates.sortedMeasurements, pantryMeasurementsNoDuplicates.noDuplicates);
     self.refrigerator = fridgeQuantities;
-    console.log(self.refrigerator);
+    self.freezer = freezerQuantities;
+    self.pantry = pantryQuantities;
+    console.log('fridge ', self.refrigerator);
+    console.log(self.freezer);
+    console.log(self.pantry);
+
+
+    let fridgeQtyNoDups = removeDups(fridgeQuantities, 'ingredientName');
+    console.log(fridgeQtyNoDups);
+    let sortFridgeQty = sortByProp(fridgeQuantities, fridgeQtyNoDups, 'ingredientName');
+    // console.log(sortFridgeQty[0].length);
+    for (let i = 0; i < sortFridgeQty.length; i++) {
+      addQuantities(sortFridgeQty[i]);
+    }
+
+    console.log(sortFridgeQty);
 
   };
 
-
-  function calculateQuantities(originalArray, noDuplicates) {
-    let arrayCopy = [];
-    console.log(noDuplicates);
-    console.log(originalArray);
-
-    for (var i = 0; i < noDuplicates.length; i++) {
-      arrayCopy.push(Object.assign({}, noDuplicates[i]));
-      let newQuantity = originalArray[i].reduce((x, y) => ({quantity: x.quantity + y.quantity}));
-      arrayCopy[i].quantity = newQuantity.quantity;
-    }
-    return arrayCopy;
-  }
-
-
-  // for (var i = 0; i < arr.length; i++) {
-  //   for (var j = 0; j < arr[i].length; j++) {
-  //     arr[i][j][0].quantity = arr[i][j].reduce((x, y) => ({quantity: x.quantity + y.quantity}));
-  //     quantitiesNoDups.push();
-  //   }
-  // }
-
-
+  // Sorts ingredients into 3 categories
+  // begin sortCategories()
   function sortCategories(ingredientsArray) {
     let refrigerator = [];
     let freezer = [];
@@ -81,17 +85,29 @@ myApp.controller('GroceryListController', ['RecipeService', function(RecipeServi
     };
 
     return sorted;
-  }
+  } // end sortCategories()
 
+  // removes duplicates from an array of objects based on the property passed in
+  // begin removeDups()
+  function removeDups(arr, prop) {
+      return arr.filter((obj, pos, arr) => {
+          return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+      });
+  } // end removeDups()
+
+  // takes in two arrays, a master array, and an array with no duplicate objects
+  // and sorts like objects into their own array based on the property passed in
+  // begin sortByProp()
   function sortByProp(originalArray, noDuplicates, property) {
     let sortedArray = [];
     for (let i = 0; i < noDuplicates.length; i++) {
       sortedArray.push(originalArray.filter((item) => item[property] == noDuplicates[i][property]));
     }
-    // console.log('split by name: ',sortedArray);
     return sortedArray;
-  }
+  } // end sortByProp()
 
+  // takes in an array of ingredients and sorts them by like measurements
+  // begin getMeasurements()
   function getMeasurements(originalArray) {
     let sortedMeasurements = [];
     let noDuplicates = [];
@@ -107,46 +123,87 @@ myApp.controller('GroceryListController', ['RecipeService', function(RecipeServi
       sortedMeasurements.push(sortByProp(originalArray[i], noDuplicates[i], 'measurement'));
     }
     // console.log('Sorted by measurement', sortedMeasurements);
+    // addQuantities(sortedMeasurements);
+
     let sorted = {
       sortedMeasurements: unNestArray(sortedMeasurements),
       noDuplicates: unNestArray(noDuplicates)
     };
 
     return sorted;
+  } // end getMeasurements()
+
+  // takes in a master array of ingredients and an array with no duplicate objects
+  // and adds like ingredient measurements
+  // begin calculateQuantities()
+  function calculateQuantities(originalArray, noDuplicates) {
+    let arrayCopy = [];
+    // console.log(noDuplicates);
+    // console.log(originalArray);
+
+    for (var i = 0; i < noDuplicates.length; i++) {
+      arrayCopy.push(Object.assign({}, noDuplicates[i]));
+      let newQuantity = originalArray[i].reduce((x, y) => ({quantity: x.quantity + y.quantity}));
+      arrayCopy[i].quantity = newQuantity.quantity;
+    }
+    return arrayCopy;
+  } // end calculateQuantities()
+
+  //
+  function addQuantities(array) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i].measurement == 'tsp' || array[i].measurement == 'tsp-fl') {
+        tspToTbsp(array[i]);
+      } else if (array[i].measurement == 'tbsp') {
+        tbspToCup(array[i]);
+      } else if (array[i].measurement == 'cup-fl') {
+        cupToFlOz(array[i]);
+      } else if (array[i].measurement == 'oz') {
+        ozToLbs(array[i]);
+      }
+    }
   }
 
+  function tspToTbsp(ingredient) {
+    if (ingredient.measurement == 'tsp-fl') {
+      if (ingredient.quantity % 3 == 0) {
+        ingredient.quantity = ingredient.quantity / 3;
+        ingredient.measurement = 'tbsp-fl';
+      }
+    } else if (ingredient.quantity % 3 == 0) {
+      ingredient.quantity = ingredient.quantity / 3;
+      ingredient.measurement = 'tbsp';
+    }
+  }
+
+  function tbspToCup(ingredient) {
+    if (ingredient.quantity % 4 == 0) {
+      ingredient.quantity = ingredient.quantity / 16;
+      ingredient.measurement = 'cup';
+    }
+  }
+
+  function cupToFlOz(ingredient) {
+    if (ingredient.quantity >= 1) {
+      ingredient.quantity *= 8;
+      ingredient.measurement = 'fl-oz';
+    }
+  }
+
+  function ozToLbs(ingredient) {
+    if (ingredient.quantity >= 16) {
+      ingredient.quantity /= 16;
+      ingredient.measurement = 'lbs';
+    }
+  }
+  // takes in an array and reduces the nesting level by one
+  // begin unNestArray()
   function unNestArray(array) {
     let unNestedArray = [];
     for (var i = 0; i < array.length; i++) {
       array[i].forEach(item => unNestedArray.push(item));
     }
     return unNestedArray;
-  }
-
-  // removes duplicates from an array of objects
-  function removeDups(arr, prop) {
-      return arr.filter((obj, pos, arr) => {
-          return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
-      });
-  }
-
-  // takes new array of unique elements and adds the quantity total of the matching items in the old array
-  // function getIngTotals() {
-  //
-  //   let newArr = removeDups(a, 'name');
-  //
-  //   for (var i = 0; i < newArr.length; i++) {
-  //     if(a.filter((x) => x.name == newArr[i].name).length > 1) {
-  //       let newQuantity = a.filter((x) => x.name == newArr[i].name).reduce((x, y) => ({quantity: x.quantity + y.quantity}));
-  //       newArr[i].quantity = newQuantity.quantity;
-  //     }
-  //   }
-  //   return newArr;
-  // }
-  console.clear();
-
-
-
-
+  } // end unNestArray()
 
 }]);
