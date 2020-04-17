f381bf87223fb94ceeecbe5e5e71b86d4a16ea57
JoakinsO <32000481+JoakinsO@users.noreply.github.com>
2020-04-17 00:44:07 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(express.static('server/public'));
 
 
 /** Listen * */
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
